fc3eb54452d0127abf1db70b0befa93e3344b801
Ivan Luchev <luchevz@gmail.com>
2019-04-11 22:33:16 +0300

    Update db_con.php

--- html/api/DBCON/db_con.php
+++ html/api/DBCON/db_con.php
@@ -4,8 +4,8 @@ class DBCON {
   const VALUES = array(
     'host' => 'localhost',
     'database-name' => 'booky',
-    'username' => '1LTLMX83COAODMXQ',
-    'password' => 'EMJehqzGH5qe9R1C',
+    'username' => '1LTLMX83COAODMXQ', // dummy account name
+    'password' => 'EMJehqzGH5qe9R1C', // dummy password
   );
 }
 
